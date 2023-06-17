e07e5324f56107b3b8d3db21883eae662556b254
SamuelP1995 <samuel_poirrier@yahoo.com>
2023-06-17 13:53:02 -0500

    finished math.js

---
+++ module-1/math.js
@@ -0,0 +1,24 @@
+function add(a,b){
+    return a+b
+}
+function subtract(a,b){
+    return a-b
+}
+function multiply(a,b){
+    return a*b
+}
+function divide(a,b){
+    return a/b
+}
+
+function hello(name){
+    const greeting = "Hello";
+    const greetingText = greeting + " " + name;
+    return greetingText
+}
+
+console.log(add(5,5))
+console.log(subtract(5,5))
+console.log(multiply(5,5))
+console.log(divide(5,5))
+console.log(hello("Samuel"))
